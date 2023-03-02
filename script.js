8c5de29907de3c3e7153e9ccdc08dab154c87d42
oasjs <oasjatoba1@gmail.com>
2023-03-01 21:22:57 -0300

    Implement play function and winner display

--- script.js
+++ script.js
@@ -7,46 +7,81 @@ function randInt(min, max) {
   return randInt;
 }
 
+function getPlayerChoice() {
+  const playerOptions = document.getElementsByName("option");
+  for (let i = 0; i < playerOptions.length; i++) {
+    if (playerOptions[i].checked) {
+      return +playerOptions[i].value;
+    }
+  }
+}
+
+function setChoiceImage() {
+  const playerChoice = getPlayerChoice();
+}
+
 function getComputerChoice() {
   return randInt(0, options.length);
 }
 
 function evaluateWinner(p1Choice, p2Choice) {
+  // If it overflows the array, go back to the start
   const next = options[p1Choice + 1] === undefined ? 0 : p1Choice + 1;
+  // If it overflows the array, go straight to the end
   const previous = options[p1Choice - 1] === undefined ? 2 : p1Choice - 1;
-
+  // The next option always wins over the previous
   if (options[p2Choice] === options[next]) return 2;
   else if (options[p2Choice] === options[previous]) return 1;
   else return 0;
 }
 
-function play(rounds = 1) {
-  for (let i = 0; i < rounds; i++) {
-    const playerChoiceInt = +prompt(`Choose:
-    [0] Rock
-    [1] Paper
-    [2] Scissors`);
-    const compChoiceInt = getComputerChoice();
-    const winner = evaluateWinner(playerChoiceInt, compChoiceInt);
-    const playerChoiceStr = options[playerChoiceInt];
-    const compChoiceStr = options[compChoiceInt];
-    alert(`Player: ${playerChoiceStr} vs Computer: ${compChoiceStr}`);
-    switch (winner) {
-      case 1:
-        alert("Player Wins!");
-        break;
-      case 2:
-        alert("Computer Wins!");
-        break;
-      default:
-        alert("Tie!");
-        break;
-    }
+function play() {
+  const playerChoice = getPlayerChoice();
+  const compChoice = getComputerChoice();
+  setComputerHandImage(compChoice);
+  const winner = evaluateWinner(playerChoice, compChoice);
+  const resultDisplay = document.querySelector("#result-display");
+  let winnerStr;
+  switch (winner) {
+    case 1:
+      winnerStr = "Player Wins!";
+      break
+    case 2:
+      winnerStr = "Computer Wins!";
+      break
+    default:
+      winnerStr = "Tie!";
+      break
+  }
+  resultDisplay.innerText = winnerStr;
+}
+
+getPlayerChoice();
+
+function setPlayerHandImage() {
+  const input = document.getElementsByName("option");
+  const img = document.getElementById("player-hand");
+  input.forEach((option) => {
+    option.addEventListener("input", updateValue);
+  });
+  function updateValue(e) {
+    img.src = `img/${e.target.value}.png`;
+    img.alt = options[e.target.value];
   }
 }
 
+function setComputerHandImage(index) {
+  const img = document.getElementById("computer-hand");
+  img.src = `img/${index}.png`;
+  img.alt = options[index];
+}
+
 function game() {
-  play(5);
+  setPlayerHandImage();
+
+  const playButton = document.querySelector("#play-button");
+  console.log(playButton);
+  playButton.addEventListener("click", play);
 }
 
 game();
